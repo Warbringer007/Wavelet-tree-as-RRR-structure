70cf6f1282aa113aa3dbc81618f9e404f6f43bf3
Chomet <marinneki@gmail.com>
2015-12-30 15:43:52 +0100

    Nodes of wavelet tree are implemented as RRR

--- wavelet_tree.cpp
+++ wavelet_tree.cpp
@@ -1,80 +1,141 @@
 #include "wavelet_tree.h"
+#include "rrr_struct.h"
 
-wavelet_tree::wavelet_tree(){
+Wavelet_tree::Wavelet_tree(std::string file) {
+    Wavelet_tree::GetStringFromFile(file);
+    std::cout << "root size: " << root.size() << std::endl;
+    std::cout << "left size: " << left.size() << std::endl;
+    std::cout << "right size: " << right.size() << std::endl;
+    int wholeroot,remainderroot, wholeleft, remainderleft, wholeright, remainderright= 0;
+    root_RRR.DefineStruct(root.size());
+    left_RRR.DefineStruct(left.size());
+    right_RRR.DefineStruct(right.size());
+    uint32_t BitsPerBlock = root_RRR.GetBitsPerBlock();
+    uint32_t BitsPerBlockL = left_RRR.GetBitsPerBlock();
+    uint32_t BitsPerBlockR = right_RRR.GetBitsPerBlock();
+    std::cout<<"Bits per block: " << BitsPerBlock<<std::endl;
+    wholeroot = root.size() / BitsPerBlock;
+    remainderroot = root.size() % BitsPerBlock;
+    wholeleft = left.size() / BitsPerBlockL;
+    remainderleft = left.size() % BitsPerBlockL;
+    wholeright = right.size() / BitsPerBlockR;
+    remainderright = right.size() % BitsPerBlockR;
+    std::cout << "cjelobrojno dijeljenjeroot: " << wholeroot << std::endl;
+    std::cout << "ostatakroot " << remainderroot << std::endl;
+    //std::cout<<root_RRR.GetBitsPerBlock()<<std::endl;
+    /*
+
+    root
+
+    */
+    for ( int i = 0; i < wholeroot; i++ ) {
+        std::vector<bool> moj;
+        for ( int j = 0; j< BitsPerBlock; j++){
+            moj.push_back(root[i*BitsPerBlock+j]);
+        }
+        root_RRR.NewBlock(moj);
+    }
+    std::vector<bool> moj;
+    for ( int i = 0; i < remainderroot; i++){
+        moj.push_back(root[wholeroot*BitsPerBlock+i]);
+    }
+    root_RRR.NewBlock(moj);
+
+    std::cout<<std::endl;
+     /*
+
+    left
+
+    */
+
+    for ( int i = 0; i < wholeleft; i++ ) {
+        std::vector<bool> mojl;
+        for ( int j = 0; j< BitsPerBlockL; j++){
+            mojl.push_back(left[i*BitsPerBlockL+j]);
+        }
+        left_RRR.NewBlock(mojl);
+    }
+    std::vector<bool> mojl;
+    for ( int i = 0; i < remainderleft; i++){
+        mojl.push_back(left[wholeleft*BitsPerBlockL+i]);
+    }
+    left_RRR.NewBlock(mojl);
+    std::cout<<std::endl;
+
+     /*
+
+    right
+
+    */
+
+    for ( int i = 0; i < wholeright; i++ ) {
+        std::vector<bool> mojr;
+        for ( int j = 0; j< BitsPerBlockR; j++){
+            mojr.push_back(right[i*BitsPerBlockR+j]);
+        }
+        right_RRR.NewBlock(mojr);
+    }
+    std::vector<bool> mojr;
+    for ( int i = 0; i < remainderright; i++){
+        mojr.push_back(right[wholeright*BitsPerBlockR+i]);
+    }
+    right_RRR.NewBlock(mojr);
+    //RRRStruct left_RRR(5);
+    //RRRStruct right_RRR(5);
+    //std::cout << root_RRR.GetBitsPerBlock() << std::endl;
+}
+
+Wavelet_tree::~Wavelet_tree(){
 
 }
 
-wavelet_tree::~wavelet_tree(){
-
-}
-
-std::string wavelet_tree:: GetStringFromFile(std::string file){
-    std::ifstream input ("example.txt");
-    //std::ifstream input(file.c_str());
-
-	std::string bzvz = "";
+std::string Wavelet_tree:: GetStringFromFile(std::string file){
+    std::ifstream input(file.c_str());
 	std::string input_string = "";
+	char one;
 
-	while (std::getline(input, bzvz)) {
-			input_string += bzvz;
+    while (input.get(one)) {
+            //std::cout << one << std::endl;
+            if ( one=='a' || one=='g') {
+                    //std::cout << "usoag" << std::endl;
+                    root.push_back(true);
+                if ( one=='a'){
+                    //std::cout << "usoa" << std::endl;
+                    left.push_back(true);
+                }
+                else left.push_back(false);
+            }
+            else {
+                //std::cout << "usotc" << std::endl;
+                root.push_back(false);
+                if ( one=='t')
+                    right.push_back(true);
+                else right.push_back(false);
+            }
+			input_string += one;
 	}
 	input.close();
-	//std::cout <<input_string<<std::endl;
+   std::string string_;
+    for ( int i = 0; i < root.size(); i++) {
+        if ( root[i]) {
+            string_.append("1");
+        } else {
+            string_.append("0");
+        }
+    }
+    /*std::cout<<string_<<std::endl;
+    std::cout<<root[0];
+    std::cout<<root[1];
+    std::cout<<root[2];
+    std::cout<<root[3]<<std::endl;
+    std::cout<<left[0];
+    std::cout<<left[1];
+    std::cout<<left[2];
+    std::cout<<left[3]<<std::endl;
+    std::cout<<right[0];
+    std::cout<<right[1];
+    std::cout<<right[2];
+    std::cout<<right[3]<<std::endl;
+	std::cout <<input_string<<std::endl;*/
 	return input_string;
-}
-
-std::vector<std::string> wavelet_tree::CreateTree(const std::string& input){
-    std::string leftnode="";
-    std::string rightnode="";
-    uint32_t string_length = input.length();
-    for ( int i = 0; i < string_length; i++) {
-        if ( input[i] == 'a' || input[i]=='g') {
-            leftnode+=input[i];
-        } else {
-            rightnode+=input[i];
-        }
-    }
-    std::vector<std::string> nodes;
-    nodes.push_back(leftnode);
-    nodes.push_back(rightnode);
-    return nodes;
-}
-
-std::vector<bool> wavelet_tree::ConvertStringToBitVectorMain(const std::string& input_string) {
-    std::vector<bool> bitvector;
-    uint32_t string_length = input_string.length();
-    for ( int i = 0; i < string_length; i++) {
-        if ( input_string[i] == 'a' ||input_string[i]=='g') {
-            bitvector.push_back(true);
-        } else {
-            bitvector.push_back(false);
-        }
-    }
-    return bitvector;
-}
-
-std::vector<bool> wavelet_tree::ConvertStringToBitVectorLeft(const std::string& input_string) {
-    std::vector<bool> bitvector;
-    uint32_t string_length = input_string.length();
-    for ( int i = 0; i < string_length; i++) {
-        if ( input_string[i] == 'a') {
-            bitvector.push_back(true);
-        } else {
-            bitvector.push_back(false);
-        }
-    }
-    return bitvector;
-}
-
-std::vector<bool> wavelet_tree::ConvertStringToBitVectorRight(const std::string& input_string) {
-    std::vector<bool> bitvector;
-    uint32_t string_length = input_string.length();
-    for ( int i = 0; i < string_length; i++) {
-        if ( input_string[i] == 't') {
-            bitvector.push_back(true);
-        } else {
-            bitvector.push_back(false);
-        }
-    }
-    return bitvector;
 }
--- wavelet_tree.h
+++ wavelet_tree.h
@@ -1,37 +1,34 @@
 #ifndef WAVELET_TREE_H
 #define WAVELET_TREE_H
 #include <stdint.h>
+#include "rrr_struct.h"
 
 #include <vector>
 #include <fstream>
 #include <string>
 #include <iostream>
 
-class wavelet_tree
+class Wavelet_tree
 {
     public:
 
         //Constructor
-        wavelet_tree();
+        Wavelet_tree(std::string file);
 
         //Destructor
-        ~wavelet_tree();
+        ~Wavelet_tree();
 
         //Get string from file
         std::string GetStringFromFile(std::string file);
 
-        //Create Wavelet Tree
-        std::vector<std::string> CreateTree(const std::string& input);
-
-        //Convert string to bitvector(main)
-        std::vector<bool> ConvertStringToBitVectorMain(const std::string& string_);
-
-        //Convert string to bitvector(left)
-        std::vector<bool> ConvertStringToBitVectorLeft(const std::string& string_);
-
-        //Convert string to bitvector(right)
-        std::vector<bool> ConvertStringToBitVectorRight(const std::string& string_);
     private:
+
+        std::vector<bool> root;
+        std::vector<bool> left;
+        std::vector<bool> right;
+        RRRStruct root_RRR;
+        RRRStruct left_RRR;
+        RRRStruct right_RRR;
 };
 
 #endif // WAVELET_TREE_H
